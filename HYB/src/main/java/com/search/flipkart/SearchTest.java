868eec02eb4c0712a822b5d11bb562f9ca41a026
sounderrajarajan@gmail.com <sounderrajarajan@gmail.com>
2020-05-08 15:46:01 +0530

    flipkart search 1st commit

---
+++ HYB/src/main/java/com/search/flipkart/SearchTest.java
@@ -0,0 +1,49 @@
+package com.search.flipkart;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SearchTest {
+
+	WebDriver driver;
+
+	@BeforeMethod
+	public void browserlaunch() {
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+
+	}
+
+	@Test
+	public void test1() {
+
+		driver.findElement(By.xpath("//*[contains(text(),'✕')]")).click();
+		driver.findElement(By.name("q")).sendKeys("iphone");
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		driver.findElement(By.name("q")).click();
+
+	}
+
+	@AfterMethod
+	public void browserclose() {
+
+		driver.quit();
+
+	}
+
+}
