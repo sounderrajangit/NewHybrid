41dc7a1747c9e4c60a775acc90547fc334356a95
sounderrajarajan@gmail.com <sounderrajarajan@gmail.com>
2020-05-08 16:37:49 +0530

    Testng.xml

--- HYB/src/main/java/com/search/flipkart/SearchTest.java
+++ HYB/src/main/java/com/search/flipkart/SearchTest.java
@@ -1,5 +1,6 @@
 package com.search.flipkart;
 
+import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -7,7 +8,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
 public class SearchTest {
 
